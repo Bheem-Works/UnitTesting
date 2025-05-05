3f597fb0c8a77fac0b12b9c1f5ce082ffb76c3f3
Bhim magar <shokokomichan098@gmail.com>
2025-05-05 10:27:26 +0545

    TestPass

--- test/max.test.js
+++ test/max.test.js
@@ -1,17 +1,19 @@
 import { describe, test, it, expect } from "vitest";
 import { strList } from "../src/max";
+import { whitespaceNumber } from "../src/WhiteSpace";
 
 
 describe('strList()', () => {
   it('should be comma seprated numbers into an array',()=>{
     expect(strList('1,2,3,4')).toEqual([1,2,3,4]);
   });
+})
 
-  it('should ignore empty value at the end' , () =>{
-    expect(strList('1,2,3,4,,')).toEqual([1,2,3,4]);
+describe('whitespaceNumber',() => {
+  it('should return correct white space fot the positive number 2 ',()=>{
+    expect(whitespaceNumber(2)).toEqual("\t,\t");
   });
-
-  it('should ignore the extra spaces like ,, and empty values' ,() => {
-    expect(strList('1,2,3,,4,5')).toEqual([1,2,3,4,5]);
+  it('should return the correct white space for the negative numer -1',()=>{
+    expect(whitespaceNumber(-1)).toEqual("\t\t")
   })
 })
