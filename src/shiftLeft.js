0eba585243c843fc3fa2338802eaa5a2c3200a64
Bhim magar <shokokomichan098@gmail.com>
2025-05-17 09:27:11 +0545

    ShiftLeftSucess

---
+++ src/shiftLeft.js
@@ -0,0 +1,13 @@
+    function shiftLeft(s,t){
+        let i = s.length-1;
+        let j = t.length-1;
+        let currValue = 0;
+
+        while(i >= 0 && j>=0 && s[i] === t[j]){
+            currValue++;
+            i--;
+            j--;
+        }
+        return (s.length - currValue) + (t.length - currValue);
+    }
+    console.log(shiftLeft('iso','t-iso'));
