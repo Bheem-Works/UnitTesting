82f041f036eb6ccc87a0e824bfe4799b121f35f6
Bhim magar <shokokomichan098@gmail.com>
2025-05-19 23:18:22 +0545

    TestPass

--- test/max.test.js
+++ test/max.test.js
@@ -1,10 +1,16 @@
 import { describe, test, it, expect } from "vitest";
 import {generateNumber} from '../src/phoneNumber'
-
+import {twoSetEqual} from '../src/TwoSetEqualSum';
 
   describe('generateNumber',()=>{
     it('should return the correct phone number',()=>{
       expect(generateNumber([1,2,3,4,5,6,7,8,9,10])).toEqual("(123 + 456 - 78910)");
     })
   })
+
+  describe("twoSetEqual",()=>{
+    it('shoud return the equal value to the both set',()=>{
+      expect(twoSetEqual(4)).toEqual([[ 4, 1 ], [ 3, 2 ] ]);
+    });
+  });
 
