d501d0ba04eed7cb340f9b6b37e4e8ca3cedf0eb
bheem-works <shokokomichan098@gmail.com>
2025-05-26 23:07:26 +0545

    ReduceJs

---
+++ src/leetCode/reduce.js
@@ -0,0 +1,10 @@
+     
+     function reduce(num,fn,init){
+        let total = init;
+        for(let i = 0; i < num.length; i++) {
+            total = fn(total,num[i])
+        }
+        return total;
+     }
+
+     console.log(reduce([1,2,3,4],(acc,curr) => acc + curr,0));
