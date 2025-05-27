87e39215a0f250b4d93482695548e942dcb43e64
bheem-works <shokokomichan098@gmail.com>
2025-05-27 14:45:05 +0545

    FabinacciSequencejavascript

---
+++ src/fabinocii.js
@@ -0,0 +1,9 @@
+     
+     function fabinacci(n) {
+        let fib = [0,1];
+        for(let i = 2; i < n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        return fib;
+     }
+     console.log(fabinacci(3));
