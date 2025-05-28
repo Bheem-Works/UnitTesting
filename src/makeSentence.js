0d1aeb1cc55099178e95ada3b28e150439d2288b
bheem-works <shokokomichan098@gmail.com>
2025-05-29 02:35:58 +0545

    SucessChange

--- src/makeSentence.js
+++ src/makeSentence.js
@@ -1,9 +1,26 @@
     export function makeSentence(string){
         let result = [];
         for(let i = 0; i < string.length; i++) {
-            if(string[i] === ' ' || string[i] === ',') continue;
-            result.push(string[i]);
+            let words = string[i];
+
+            if(words === ',') {
+                if(result.length > 0) {
+                    result[result.length - 1] += ',';
+                }                
+            }
+            else if(words === '.'  || words === '.' || words === '...')  {
+                continue;
+
+            } else {
+                result.push(words)
+            }
         }
-        return result.join(' ') + '.';
+        let sentences = result.join(' ').replace(/\.*$/, "") + '.';
+        return sentences;
+        
     }
-    console.log(makeSentence(['hello', '', 'nima']));
+    console.log(makeSentence(['hello', ',' ,'nima']));
+console.log(makeSentence(['hello', ',', 'my', 'dear']));         // "hello, my dear."
+console.log(makeSentence(['hello', 'world']))                   // "hello world."
+console.log(makeSentence(['hello', 'world', '.', '.', '..']))   // "hello world."
+console.log(makeSentence(['yo', ',', 'fam', ',', 'how', 'you', 'doing', '...'])) // "yo, fam, how you doing."
