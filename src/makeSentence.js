06c6840b6ac23b206efd34741e0ee926e2aaa162
bheem-works <shokokomichan098@gmail.com>
2025-05-29 15:53:51 +0545

    PushToday

--- src/makeSentence.js
+++ src/makeSentence.js
@@ -1,26 +1,18 @@
-    export function makeSentence(string){
+    function makeSentence(str) {
         let result = [];
-        for(let i = 0; i < string.length; i++) {
-            let words = string[i];
+        for(let i = 0; i < str.length; i++) {
+            let words = str[i];
 
             if(words === ',') {
-                if(result.length > 0) {
+                if(result.length > 0 ) {
                     result[result.length - 1] += ',';
-                }                
-            }
-            else if(words === '.'  || words === '.' || words === '...')  {
+                }
+            } else if(words === '.' || words === '..' || words === '...') {
                 continue;
-
             } else {
-                result.push(words)
+                result.push(words);
             }
         }
-        let sentences = result.join(' ').replace(/\.*$/, "") + '.';
-        return sentences;
-        
+        let sentence = result.join(' ') + '.';
+        return sentence;
     }
-    console.log(makeSentence(['hello', ',' ,'nima']));
-console.log(makeSentence(['hello', ',', 'my', 'dear']));         // "hello, my dear."
-console.log(makeSentence(['hello', 'world']))                   // "hello world."
-console.log(makeSentence(['hello', 'world', '.', '.', '..']))   // "hello world."
-console.log(makeSentence(['yo', ',', 'fam', ',', 'how', 'you', 'doing', '...'])) // "yo, fam, how you doing."
