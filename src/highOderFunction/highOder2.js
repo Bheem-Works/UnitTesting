21d9ac71c6fcc49db93db4b2ab0a11650e6b9f5f
bheem-works <shokokomichan098@gmail.com>
2025-06-02 08:45:13 +0545

    commit

---
+++ src/highOderFunction/highOder2.js
@@ -0,0 +1,8 @@
+        function greetDevelopers(list){
+            return list.map(dev => ({
+                ...dev,
+                greeting:`Hi${dev.firstName}, what do you like most about ${dev.langauge}?`
+            }))
+        }
+
+        
